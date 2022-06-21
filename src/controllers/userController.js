2252196cb57139db0c14d7638362f128e0a375da
bohongu <jaehong2782@naver.com>
2022-06-21 13:44:50 +0900

    Play & Pause

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -223,7 +223,7 @@ export const postChangePassword = async (req, res) => {
 export const see = async (req, res) => {
   const { id } = req.params;
   const user = await User.findById(id).populate({
-    path: "vidoes",
+    path: "videos",
     populate: { path: "owner", model: "User" },
   });
   if (!user) {
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,19 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/client/js/main.js", //File that we want to transform
+  entry: {
+    main: "./src/client/js/main.js",
+    videoPlayer: "./src/client/js/videoPlayer.js",
+  }, //File that we want to transform
+  mode: "development",
   watch: true,
   plugins: [
     new MiniCssExtractPlugin({
       filename: "css/styles.css",
     }),
   ],
-  mode: "development",
   output: {
-    filename: "js/main.js",
+    filename: "js/[name].js",
     path: path.resolve(__dirname, "assets"),
     clean: true,
   },
