f9c1cddaa83e0df84ab9a5063ae140cea5ed45de
bohongu <jaehong2782@naver.com>
2022-06-21 13:55:39 +0900

    Mute & Unmute

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,7 @@ const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
-const volume = document.getElementById("volume");
+const volumeRange = document.getElementById("volume");
 
 const handlePlayClick = (event) => {
   if (video.paused) {
@@ -10,18 +10,18 @@ const handlePlayClick = (event) => {
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
-const handlePause = () => {
-  playBtn.innerText = "Play";
+const handleMute = (event) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "UnMute" : "Mute";
+  volumeRange.value = video.muted ? 0 : 0.5;
 };
-const handlePlay = () => {
-  playBtn.innerText = "Pause";
-};
-
-const handleMute = (event) => {};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
