458955121d6c14b9d2a4b4f0d997b9831fe50ade
bohongu <jaehong2782@naver.com>
2022-06-23 16:00:26 +0900

    Start RECORD

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   entry: {
     main: "./src/client/js/main.js",
     videoPlayer: "./src/client/js/videoPlayer.js",
+    recorder: "./src/client/js/recorder.js",
   }, //File that we want to transform
   mode: "development",
   watch: true,
